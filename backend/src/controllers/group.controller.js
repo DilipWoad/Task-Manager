f617fa6beee429710524d8d834972ff81d6f3685
Dilip Woad <dilipwoad777@gmail.com>
2025-12-03 16:32:30 +0530

    create group model and add goup controllers structure.

---
+++ backend/src/controllers/group.controller.js
@@ -0,0 +1,17 @@
+const createGroup = () => {};
+
+const deleteGroup = () => {};
+
+const editGroupName = () => {};
+
+const addUserToGroup = () => {};
+
+const removeUserFromGroup = () => {};
+
+export {
+  createGroup,
+  deleteGroup,
+  editGroupName,
+  addUserToGroup,
+  removeUserFromGroup,
+};
