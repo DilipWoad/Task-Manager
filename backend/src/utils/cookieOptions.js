b22f028abbffc99d3ea9165ed58d9ca385363214
Dilip Woad <dilipwoad777@gmail.com>
2026-01-17 22:55:22 +0530

    backend: set cookies sameSite to None

--- backend/src/utils/cookieOptions.js
+++ backend/src/utils/cookieOptions.js
@@ -3,15 +3,15 @@ import { map } from "zod";
 const AccessTokenOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "Strict",
   // sameSite: "Strict",
+  sameSite: "None",
   maxAge: 16* 60 * 60 * 1000,
 };
 const RefreshTokenOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "Strict",
   // sameSite: "Strict",
+  sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 export {AccessTokenOptions,RefreshTokenOptions};
