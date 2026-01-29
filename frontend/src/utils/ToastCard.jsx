063ea787b4ce55bf734f4f00602b7c43b23ed61d
Dilip Woad <dilipwoad777@gmail.com>
2026-01-29 22:10:10 +0530

    fix toast card not showing up

--- frontend/src/utils/ToastCard.jsx
+++ frontend/src/utils/ToastCard.jsx
@@ -1,7 +1,8 @@
 const ToastCard = ({ animation, toastCardMessage, toastCss }) => {
+  console.log("Does card is called?")
   return (
     <p
-      className={`${toastCss} text-nowrap absolute top-0 left-1/2 -translate-x-1/2 mt-2 bg-gray-900 text-gray-100 p-4 text-sm rounded-md transition-all duration-1000 z-50 ${
+      className={`${toastCss} wrap-break-word w-auto absolute top-0 left-1/2 -translate-x-1/2 mt-2 bg-primaryColor text-quaternaryColor font-semibold text-center p-4 text-sm sm:text-lg rounded-md transition-all duration-1000 z-50${
         animation ? "opacity-0 -translate-y-4" : "opacity-100 translate-y-0"
       }`}
     >
