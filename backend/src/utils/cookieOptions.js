f38fc482836d288a00f0b999a19809325a111c63
Dilip Woad <dilipwoad777@gmail.com>
2026-02-01 03:27:28 +0530

    backend:setting cookie option secure to production

--- backend/src/utils/cookieOptions.js
+++ backend/src/utils/cookieOptions.js
@@ -2,14 +2,14 @@ import { map } from "zod";
 
 const AccessTokenOptions = {
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV,
   // sameSite: "Strict",
   sameSite: "None",
   maxAge: 16* 60 * 60 * 1000,
 };
 const RefreshTokenOptions = {
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV,
   // sameSite: "Strict",
   sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000,
