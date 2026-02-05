8fc35449449b386489a9050f0215909c15dd3f92
Dilip Woad <dilipwoad777@gmail.com>
2026-02-05 15:43:43 +0530

    refactor : app and config files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { GlobalErrorHandler } from "./utils/GlobalErrorHandler.js";
 import cors from "cors";
+import { CROSS_ORIGIN } from "./config.js";
 
 export const app = express();
 
 //configure express here]
-console.log(process.env.CROSS_ORIGIN)
 app.use(
   cors({
-    origin: process.env.CROSS_ORIGIN,
+    origin: CROSS_ORIGIN,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
@@ -30,6 +30,7 @@ import authRoute from "./routes/auth.route.js";
 import taskRoute from "./routes/task.route.js";
 import groupRoute from "./routes/group.route.js";
 import userRoute from "./routes/user.route.js";
+
 
 app.use("/api/v1/auths", authRoute);
 app.use("/api/v1/tasks",taskRoute);
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,11 +4,18 @@ dotenv.config({
     path: '.env'
 })
 
-
+//Import env here
 const _MONGO_URI=process.env.MONGODB_URI;
+const _CROSS_ORIGIN=process.env.CROSS_ORIGIN;
+
 //VALIDATE IT HERE
 if(!_MONGO_URI){
-    throw new Error("FATAL ERROR: MONGODB_URI is not defined in the .env file")
+    throw new Error("FATAL ERROR : MONGODB_URI is not defined in the .env file")
+}
+if(!_CROSS_ORIGIN){
+    throw new Error("ORIGIN ERROR : CROSS_ORIGIN is not defined in the .env file")
 }
 
+
 export const MONGO_URI=_MONGO_URI
+export const CROSS_ORIGIN=_CROSS_ORIGIN
